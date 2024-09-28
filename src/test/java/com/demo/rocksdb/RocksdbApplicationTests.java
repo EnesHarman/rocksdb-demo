970f1d7a47509872670bd211a8ef0cf24d72debf
enesharman <enesharman45@gmail.com>
2024-09-28 18:03:59 +0300

    Initial Commit

---
+++ src/test/java/com/demo/rocksdb/RocksdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.rocksdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocksdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
